85da0116f75040023f1243c1be45e7ed28172879
boskuv <boris_kv@bk.ru>
2022-10-29 16:30:01 +0300

    feat: handler for ini file

---
+++ config.py
@@ -0,0 +1,28 @@
+"""Парсер конфигурации бота"""
+import configparser
+from dataclasses import dataclass
+
+
+@dataclass
+class TgBot:
+    token: str
+    admin_id: int
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str):
+    config = configparser.ConfigParser()
+    config.read(path)
+
+    tg_bot = config["tg_bot"]
+
+    return Config(
+        tg_bot=TgBot(
+            token=tg_bot.get("token"),
+            admin_id=tg_bot.getint("admin_id"),
+        ),
+    )
