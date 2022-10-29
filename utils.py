484581854cba665d61a1f4b801b2ccb7fada111b
boskuv <boris_kv@bk.ru>
2022-10-29 16:29:33 +0300

    refactor: move common functions to utils

--- utils.py
+++ utils.py
@@ -1,13 +1,32 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
-
+import datetime
 import re
+import pytz
 
+
+# TODO: naming
 class FinCheckerState(StatesGroup):
-    s1 = State()
-    s2 = State()
+    getting_list_of_expense_categories = State()
+    choosing_expense_category = State()
+    getting_list_of_cards = State()
+    choosing_deposit_card = State()
+    choosing_deposit_description = State()
+
+
+def get_now_formatted() -> str:
+    """Возвращает сегодняшнюю дату строкой"""
+    return get_now_datetime().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def get_now_datetime() -> datetime.datetime:
+    """Возвращает сегодняшний datetime с учётом временной зоны МСК"""
+    tz = pytz.timezone("Europe/Moscow")
+    now = datetime.datetime.now(tz)
+    return now
+
 
 def is_valid(input_expense):
-    if re.match(r'(\+|\-)\d+', input_expense) is not None:
+    if re.match(r"(\+|\-)\d+", input_expense) is not None:
         return True
     else:
         return False
